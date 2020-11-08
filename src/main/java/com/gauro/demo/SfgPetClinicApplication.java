13ff2e3ab4f469b37137bddc08b3593f152b8bc7
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-07 19:08:27 -0500

    First

--- src/main/java/com/gauro/demo/SfgPetClinicApplication.java
+++ src/main/java/com/gauro/demo/SfgPetClinicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
