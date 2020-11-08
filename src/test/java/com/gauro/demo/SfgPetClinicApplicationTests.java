63e4d05e60d1f10ad49b877731b7d4db2273404b
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-07 19:07:48 -0500

    First

---
+++ src/test/java/com/gauro/demo/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
