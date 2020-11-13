cb7a1511d50c26189cbb18c92557b4a7d0900ce6
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-12 20:35:27 -0500

    Section 5 Pet Thymeleaf Lect 82

--- pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.gauro.sfgpetclinic.controllers;
 
+import com.gauro.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"","index","index.html"})
-    public  String listOwners(){
+    public  String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
