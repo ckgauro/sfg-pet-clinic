a47e00318e8b150700b4e729e35e4b47a0d8ad1f
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-13 17:26:21 -0500

    Section 7 Pet Clinic Web for Owner Lect 120

--- pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/Owner.java
@@ -6,6 +6,40 @@ import java.util.Set;
  * @author Chandra
  */
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
