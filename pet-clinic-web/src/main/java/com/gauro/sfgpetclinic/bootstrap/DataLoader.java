92eb799fe86248fb77fe58ac3f34436c717a6202
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-13 17:44:40 -0500

    Section 7 Pet Clinic Web for DataLoader PetTypeServiceMap and PetTypeService Lect 122

--- pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.gauro.sfgpetclinic.bootstrap;
 
 import com.gauro.sfgpetclinic.model.Owner;
+import com.gauro.sfgpetclinic.model.PetType;
 import com.gauro.sfgpetclinic.model.Vet;
 import com.gauro.sfgpetclinic.services.OwnerService;
+import com.gauro.sfgpetclinic.services.PetTypeService;
 import com.gauro.sfgpetclinic.services.VetServices;
 import com.gauro.sfgpetclinic.services.map.OwnerServiceMap;
 import com.gauro.sfgpetclinic.services.map.VetServiceMap;
@@ -18,10 +20,12 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private  final OwnerService ownerService;
     private final VetServices vetServices;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetServices vetServices) {
+    public DataLoader(OwnerService ownerService, VetServices vetServices, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetServices = vetServices;
+        this.petTypeService = petTypeService;
     }
 //    public DataLoader(){
 //        ownerService=new OwnerServiceMap();
@@ -30,6 +34,15 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog=new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType=petTypeService.save(dog);
+
+        PetType cat=new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType=petTypeService.save(cat);
+
         Owner owner1=new Owner();
        // owner1.setId(1L);
         owner1.setFirstName("Chandra");
