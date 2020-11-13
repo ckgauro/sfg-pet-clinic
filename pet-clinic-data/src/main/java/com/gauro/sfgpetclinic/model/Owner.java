caa111858292342f0bd590d905b3b60c942bc041
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-13 17:19:49 -0500

    Section 7 Pet Clinic Web for visit Lect 118

--- pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/Owner.java
@@ -1,7 +1,11 @@
 package com.gauro.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Chandra
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
 }
---
+++ pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/Visit.java
@@ -0,0 +1,36 @@
+package com.gauro.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Chandra
+ */
+public class Visit  extends BaseEntity{
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
