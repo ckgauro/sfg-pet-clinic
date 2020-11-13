4a5003ec964e1a38ce3f2bcd27f1e4185dc71ce2
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-12 20:42:27 -0500

    Section 5 Pet Thymeleaf for vet Lect 83

--- pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/gauro/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.gauro.sfgpetclinic.controllers;
 
+import com.gauro.sfgpetclinic.services.VetServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("vets")
 @Controller
 public class VetController {
+    private final VetServices vetServices;
+
+    public VetController(VetServices vetServices) {
+        this.vetServices = vetServices;
+    }
 
     @RequestMapping({"","index","index.html","vets.html"})
-    public String ListVets(){
+    public String ListVets(Model model){
 
+        model.addAttribute("vets", vetServices.findAll());
         return "vets/index";
     }
 }
