b745f856c66ec67de69093c89d7da9a52d6a3df2
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-16 05:47:04 -0500

    Section 8 Pet Clinic Web Lect 145 JPA BaseEntity

--- pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/gauro/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,20 @@
 package com.gauro.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author Chandra
  */
+
+@MappedSuperclass
 public class BaseEntity  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
